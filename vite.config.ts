c1a87c0df545758c5ce8d7ac186ecb75b5a85729
QuanWenG <154224091+QuanWenG@users.noreply.github.com>
2026-06-30 22:57:01 +0800

    网页性能优化

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    manifest: true,
+    chunkSizeWarningLimit: 1000,
+  },
   test: {
     environment: 'jsdom',
     setupFiles: ['./src/test/setup.ts'],
